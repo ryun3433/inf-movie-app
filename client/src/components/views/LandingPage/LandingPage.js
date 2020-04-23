7d1d728e6106281b4eabf59ac56e7f9714764ccc
unknown <patmos3433@gmail.com>
2020-04-23 19:59:33 +0900

    init

---
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,38 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+import { withRouter } from "react-router-dom";
+
+function LandingPage(props) {
+  useEffect(() => {
+    axios.get("/api/hello").then((response) => {
+      console.log(response);
+    });
+  }, []);
+
+  const onClickHandler = () => {
+    axios.get("/api/users/logout").then((response) => {
+      if (response.data.success) {
+        props.history.push("/login");
+      } else {
+        alert("로그아웃에 실패했습니다.");
+      }
+    });
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <h2>LandingPage</h2>
+      <button onClick={onClickHandler}>Log out</button>
+    </div>
+  );
+}
+
+export default withRouter(LandingPage);
